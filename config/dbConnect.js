9c45b409ab14dc32b7d897436b09dec11b4fe266
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2024-06-24 22:15:07 +0530

    still

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./config.env" });
 
 const dbConnect = () => {
 
-  const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://hrirbno:OcfOgUqm3dxbWuca@cluster0.mongodb.net/TaskManager?retryWrites=true";
+  const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://hrirbno:OcfOgUqm3dxbWuca@cluster0.mongodb.net/TaskManager?retryWrites=true&w=majority";
 
 
   mongoose.connect(MONGODB_URL, {
@@ -12,7 +12,7 @@ const dbConnect = () => {
   }).then(() => {
     console.log("database connection successfull !!!");
   }).catch((err) => {
-    console.log(`error while connecting database !!! ${err}`);
+    console.log(`error while connecting database ${err}`);
   });
 };
 
